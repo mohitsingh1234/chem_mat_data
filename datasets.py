3d2b61f2d2ab6ce34d88d785f372c538598c7c45
mohitsingh1234 <154425011+mohitsingh1234@users.noreply.github.com>
2024-02-13 23:07:38 +0100

    Add files via upload
    Updated datasets.py with another dataset for testing.
    pipeline.py can now process multiple prompts at the same time!

--- datasets.py
+++ datasets.py
@@ -1,2 +1,2 @@
 #This list contains all the available datasets. It will be much bigger in the future.
-available_datasets = ['HIV', 'toxcast_data']
+available_datasets = ['HIV', 'toxcast_data','bace']
--- pipeline.py
+++ pipeline.py
@@ -15,10 +15,10 @@ from datasets import available_datasets
 base_url = 'https://bwsyncandshare.kit.edu/s/egMpWo6KiBRQGdF/download?path=%2F&files='
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.argument('filename', type=str, required=False)
+@click.argument('filenames', nargs=-1, type=str, required=False)
 @click.option('-check', is_flag=True, help= 'List available datasets')
 @click.option('-a', is_flag=True, help = 'Downloads all available datasets')
-def main(filename, check, a):
+def main(filenames, check, a):
     """
     Prototype Pipeline: A tool for downloading datasets.
 
@@ -29,7 +29,7 @@ def main(filename, check, a):
     if a:
         click.confirm('This will donwload the whole collection of datasets to your current working directory. Are you sure?', abort=True)
         try:
-            download_dataset(base_url, 'Dataset.zip')
+            download_dataset(base_url, 'Datasets.zip')
             click.echo(f'Collection downloaded succesfully')
         except Exception as e:
             logging.error(f'Error downloading collection : {e}')
@@ -43,40 +43,41 @@ def main(filename, check, a):
         return
 
     #If no argument is provided, it will prompt the user to give one or use an option flag
-    if not filename:
+    if not filenames:
         click.echo('Please provide a dataset name or use the "check" option to see available datasets or use "--help" option.')
         return
-    
-    #Strip it of whitespace 
-    filename = filename.strip()
 
-    # We need to check if the dataset actually exists
-    if filename not in available_datasets:
-        click.echo(f'Dataset {filename} not found. Available datasets can be seen via "-check" option')
-        return
-    
-    ### From here on out, the code aims to download the file as specified by the user ####
-    
-    # Add the ".csv" extension, wich is needed to construct the url
-    filename = filename if filename.endswith('.csv') else f'{filename}.csv'
-    # Create 'datasets' folder if it doesn't exist. All datasets are stored in that folder
     datasets_folder = 'datasets'
-    if not os.path.exists(datasets_folder):
-        os.makedirs(datasets_folder)
+    for file in filenames:
+        #Strip it of whitespace 
+        file = file.strip()
 
-    # This is the path where the dataset will be
-    destination = os.path.join(datasets_folder, filename)
-    if os.path.exists(destination):
-        click.confirm('File already exists. Do you want to overwrite?', abort=True)
+        # We need to check if the dataset actually exists
+        if file not in available_datasets:
+            click.echo(f'Dataset {file} not found. Available datasets can be seen via "-check" option')
+            continue
+        
+        ### From here on out, the code aims to download the file as specified by the user ####
+        
+        # Add the ".csv" extension, wich is needed to construct the url
+        file = file if file.endswith('.csv') else f'{file}.csv'
+        # Create 'datasets' folder if it doesn't exist. All datasets are stored in that folder
+        if not os.path.exists(datasets_folder):
+            os.makedirs(datasets_folder)
 
-    # Determine the url for the dataset
-    dataset_url = base_url + filename
-    try:
-        download_dataset(dataset_url, destination)
-        click.echo(f'Dataset downloaded succesfully to {destination}')
-    except Exception as e:
-        logging.error(f'Error downloadign dataset: {e}')
-        click.echo(f'Failed to download dataset: {e}')
+        # This is the path where the dataset will be
+        destination = os.path.join(datasets_folder, file)
+        if os.path.exists(destination):
+            click.confirm('File already exists. Do you want to overwrite?', abort=True)
+
+        # Determine the url for the dataset
+        dataset_url = base_url + file
+        try:
+            download_dataset(dataset_url, destination)
+            click.echo(f'Dataset downloaded succesfully to {destination}')
+        except Exception as e:
+            logging.error(f'Error downloadign dataset: {e}')
+            click.echo(f'Failed to download dataset: {e}')
 
 if __name__ == '__main__':
     main()
